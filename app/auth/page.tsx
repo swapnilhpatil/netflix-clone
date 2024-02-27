fd8c32e1e4b7913c8cdd160b10c3ac83ff3ec319
Swapnil <swapnilhpatil@gmail.com>
2024-02-27 17:02:52 +0530

    Initial commit: Added project files

---
+++ app/auth/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Auth() {
+  return (
+    <div className="relative h-full w-full bg-[url(/images/background.jpg)] bg-no-repeat bg-center bg-fixed bg-cover"></div>
+  );
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <h1 className="text-2xl text-green-500">Hello, Dashboard Page!</h1>
+  );
+}
